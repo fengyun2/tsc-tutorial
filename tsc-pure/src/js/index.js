bf3595bc52cce52ac59469dc6d0a778fec34eb4d
fengyun2 <2816708306@qq.com>
2016-10-16 23:40:45 +0800

    add an application of tsc-pure

---
+++ tsc-pure/src/js/index.js
@@ -0,0 +1,6 @@
+function sayHello(name) {
+    return `Hello, ${name}`;
+}
+let myName = 'Ly';
+console.log(sayHello(myName));
+console.log('1111');
---
+++ tsc-pure/src/ts/index.ts
@@ -0,0 +1,6 @@
+function sayHello(name: string) {
+  return `Hello, ${name}`
+}
+let myName = 'Ly'
+console.log(sayHello(myName))
+console.log('1111');
